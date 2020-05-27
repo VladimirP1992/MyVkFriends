39155947ee7e22a48cf30997e5bdc70727b00bc7
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-05-27 19:11:00 +0300

    После возврата из активити настроек, активити пересоздается с учетом темы

--- app/src/main/java/ru/geekbrains/myvkfriends/ui/activity/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myvkfriends/ui/activity/MainActivity.kt
@@ -1,10 +1,17 @@
 package ru.geekbrains.myvkfriends.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 import moxy.presenter.ProvidePresenter
 import ru.geekbrains.myvkfriends.R
+import ru.geekbrains.myvkfriends.mvp.model.PreferenceConstants
 import ru.geekbrains.myvkfriends.mvp.presenter.MainPresenter
 import ru.geekbrains.myvkfriends.mvp.view.MainView
 import ru.geekbrains.myvkfriends.ui.App
@@ -14,6 +21,8 @@ import ru.terrakok.cicerone.android.support.SupportAppNavigator
 import javax.inject.Inject
 
 class MainActivity : MvpAppCompatActivity(), MainView {
+
+    val REQUEST_CODE = 11
 
     @Inject
     lateinit var navigatorHolder: NavigatorHolder
@@ -30,8 +39,33 @@ class MainActivity : MvpAppCompatActivity(), MainView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme()
+
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
         App.instance.appComponent.inject(this)
+    }
+
+    private fun setTheme(){
+        val isLightTheme = getSharedPreferences(PreferenceConstants.SHARED_PREFFERENCES_NAME, Context.MODE_PRIVATE)
+            .getBoolean(PreferenceConstants.LIGHT_THEME_SWITCH_STATE, false)
+        when(isLightTheme){
+            true -> {setTheme(R.style.AppThemeLight)
+            }
+            else -> {setTheme(R.style.AppTheme)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?) =
+        MenuInflater(this).inflate(R.menu.main_menu, menu).let { true }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.settings -> openSettings()
+        else -> false
     }
 
     override fun onResumeFragments() {
@@ -51,5 +85,16 @@ class MainActivity : MvpAppCompatActivity(), MainView {
             }
         }
         presenter.backClicked()
+    }
+
+    private fun openSettings(): Boolean {
+        startActivityForResult(Intent(this, SettingsActivity::class.java), REQUEST_CODE)
+        return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE)
+            recreate()
     }
 }
